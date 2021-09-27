e956aa59a784632e9b11ecbfe80839668f8e53c2
Zazsona <40435178+Zazsona@users.noreply.github.com>
2021-09-27 22:38:38 +0100

    Limit player and mob actions that can be performed when in negotiation

--- src/main/java/com/zazsona/mobnegotiation/NegotiationEntityEventListener.java
+++ src/main/java/com/zazsona/mobnegotiation/NegotiationEntityEventListener.java
@@ -5,13 +5,14 @@ import org.bukkit.entity.Creature;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Mob;
 import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
-import org.bukkit.event.HandlerList;
-import org.bukkit.event.Listener;
+import org.bukkit.event.*;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.entity.EntityTargetEvent;
-import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryInteractEvent;
+import org.bukkit.event.player.*;
+import org.bukkit.scheduler.BukkitTask;
 
 import java.util.List;
 
@@ -19,26 +20,41 @@ public class NegotiationEntityEventListener implements Listener, NegotiationEven
 {
     private NegotiationProcess negotiation;
 
+    private Location playerLocation;
+    private Location mobLocation;
+    private float previousPlayerWalkSpeed = 0.2f;
+    private boolean previousMobAwareState = true;
+    private boolean previousPlayerInvulnerableState = false;
+    private boolean previousMobInvulnerableState = false;
+    private BukkitTask tickTask;
+
     public NegotiationEntityEventListener(NegotiationProcess negotiation)
     {
         this.negotiation = negotiation;
         this.negotiation.addEventListener(this);
     }
 
+    /*
+     * Prevent Damage
+     */
+
     /**
-     * Prevents the player and mob associated with the {@link NegotiationProcess} taking damage
+     * Prevents the player and mob associated with the {@link NegotiationProcess} taking or dealing damage
      * @param e the event
      */
     @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
     public void onMobAttack(EntityDamageByEntityEvent e)
     {
         Entity entity = e.getEntity();
-        if (entity == negotiation.getPlayer() || entity == negotiation.getMob())
+        Entity damager = e.getDamager();
+        Player player = negotiation.getPlayer();
+        Mob mob = negotiation.getMob();
+        if ((entity == player || entity == mob) || (damager == player || damager == mob))
             e.setCancelled(true);
     }
 
     /**
-     * Stops mobs targeting the negotiating player
+     * Stops mobs targeting the negotiating entities
      * @param e the target event
      */
     @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
@@ -49,6 +65,10 @@ public class NegotiationEntityEventListener implements Listener, NegotiationEven
             e.setCancelled(true);
     }
 
+    /*
+    * Prevent Movement
+    */
+
     /**
      * Stops the negotiating player from moving
      * @param e the movement event
@@ -56,10 +76,98 @@ public class NegotiationEntityEventListener implements Listener, NegotiationEven
     @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
     public void onPlayerMove(PlayerMoveEvent e)
     {
+        if (playerLocation == null)
+            return;
         Player player = e.getPlayer();
-        Location originalPosition = e.getFrom();
         if (player == negotiation.getPlayer())
-            e.setTo(originalPosition);
+        {
+            Location newPosition = new Location(playerLocation.getWorld(),
+                                                playerLocation.getX(),
+                                                playerLocation.getY(),    // Maintain original position, but enable head movement
+                                                playerLocation.getZ(),
+                                                e.getTo().getYaw(),
+                                                e.getTo().getPitch());
+            e.setTo(newPosition);
+        }
+    }
+
+    /**
+     * Runs every server tick to handle entity states
+     */
+    public void onTick()
+    {
+        Mob mob = negotiation.getMob();
+        if (mobLocation != null && !mob.getLocation().equals(mobLocation))
+            mob.teleport(mobLocation, PlayerTeleportEvent.TeleportCause.PLUGIN);
+    }
+
+    /**
+     * Stops players interacting with objects while negotiating
+     * @param e the event
+     */
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+    public void onPlayerInteract(PlayerInteractEvent e)
+    {
+        if (e.getPlayer() == negotiation.getPlayer())
+        {
+            e.setUseInteractedBlock(Event.Result.DENY);
+            e.setUseItemInHand(Event.Result.DENY);
+            e.setCancelled(true);
+        }
+    }
+
+    /**
+     * Stops players interacting with objects while negotiating
+     * @param e the event
+     */
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+    public void onPlayerConsume(PlayerItemConsumeEvent e)
+    {
+        if (e.getPlayer() == negotiation.getPlayer())
+            e.setCancelled(true);
+    }
+
+    /**
+     * Stops players interacting with objects while negotiating
+     * @param e the event
+     */
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+    public void onPlayerUseInventory(InventoryClickEvent e)
+    {
+        if (e.getWhoClicked() instanceof Player)
+        {
+            Player clicker = (Player) e.getWhoClicked();
+            if (clicker == negotiation.getPlayer())
+                e.setCancelled(true);
+        }
+
+    }
+
+    /*
+    * Terminating events
+    */
+
+    /**
+     * Stops the negotiation when the player leaves.
+     * @param e the event
+     */
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+    public void onPlayerLeave(PlayerQuitEvent e)
+    {
+        if (e.getPlayer() == negotiation.getPlayer())
+            negotiation.stop();
+    }
+
+    /**
+     * Stops the negotiation when the player or mob dies.
+     * @param e the event
+     */
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+    public void onEntityDeath(EntityDeathEvent e)
+    {
+        Entity entity = e.getEntity();
+        if (entity == negotiation.getPlayer() || entity == negotiation.getMob())
+            negotiation.stop();
     }
 
     /**
@@ -70,33 +178,54 @@ public class NegotiationEntityEventListener implements Listener, NegotiationEven
     public void onNegotiationStateUpdate(NegotiationProcess negotiation)
     {
         NegotiationState state = negotiation.getState();
+        Player player = negotiation.getPlayer();
+        Mob mob = negotiation.getMob();
         if (state == NegotiationState.STARTED)
         {
+            playerLocation = player.getLocation();
+            mobLocation = mob.getLocation();
+            previousPlayerWalkSpeed = player.getWalkSpeed();
+            previousMobAwareState = mob.isAware();
+            previousPlayerInvulnerableState = player.isInvulnerable();
+            previousMobInvulnerableState = mob.isInvulnerable();
+            player.setWalkSpeed(0);
+            mob.setAware(false);
+            player.setInvulnerable(true);
+            mob.setInvulnerable(true);
+            removeNearbyEntityTargets();
+
             MobNegotiationPlugin plugin = MobNegotiationPlugin.getInstance();
             plugin.getServer().getPluginManager().registerEvents(this, plugin);
-            removeEntityTargetsOnPlayer(negotiation.getPlayer());
+            tickTask = plugin.getServer().getScheduler().runTaskTimer(plugin, this::onTick, 1, 1);
         }
         else if (state.isTerminating)
         {
+            if (tickTask != null)
+                tickTask.cancel();
             HandlerList.unregisterAll(this);
             negotiation.removeEventListener(this);
+            player.setWalkSpeed(previousPlayerWalkSpeed);
+            mob.setAware(previousMobAwareState);
+            player.setInvulnerable(previousPlayerInvulnerableState);
+            mob.setInvulnerable(previousMobInvulnerableState);
         }
     }
 
     /**
-     * Stops all nearby entities from targeting the supplied player
-     * @param player the player to untarget
+     * Stops all nearby entities from targeting entities involved in this the negotiation
      */
-    private void removeEntityTargetsOnPlayer(Player player)
+    private void removeNearbyEntityTargets()
     {
         int entityTargetingDistance = 40;
+        Player player = negotiation.getPlayer();
+        Mob mob = negotiation.getMob();
         List<Entity> nearbyEntities = player.getNearbyEntities(entityTargetingDistance, entityTargetingDistance, entityTargetingDistance);
         for (Entity entity : nearbyEntities)
         {
             if (entity instanceof Creature)
             {
                 Creature creature = (Creature) entity;
-                if (creature.getTarget() == player)
+                if (creature.getTarget() == player || creature.getTarget() == mob)
                     creature.setTarget(null);
             }
         }
