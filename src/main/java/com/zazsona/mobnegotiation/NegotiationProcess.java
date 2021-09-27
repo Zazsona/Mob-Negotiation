a3c9fa52d93c95e1f3c2c2953db8d600be86fd52
Zazsona <40435178+Zazsona@users.noreply.github.com>
2021-09-27 22:39:44 +0100

    Add method to position entities at the start of negotiation

--- src/main/java/com/zazsona/mobnegotiation/NegotiationProcess.java
+++ src/main/java/com/zazsona/mobnegotiation/NegotiationProcess.java
@@ -2,9 +2,14 @@ package com.zazsona.mobnegotiation;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Mob;
 import org.bukkit.entity.Player;
 
+import java.security.InvalidParameterException;
 import java.util.ArrayList;
 
 public class NegotiationProcess
@@ -12,7 +17,7 @@ public class NegotiationProcess
     private Player player;
     private Mob mob;
     private NegotiationState state;
-    private ArrayList<NegotiationEvent> listeners;
+    private ArrayList<NegotiationEventListener> listeners;
 
     public NegotiationProcess(Player player, Mob mob)
     {
@@ -54,7 +59,7 @@ public class NegotiationProcess
      * @param listener the listener to add
      * @return boolean on success
      */
-    public boolean addEventListener(NegotiationEvent listener)
+    public boolean addEventListener(NegotiationEventListener listener)
     {
         return listeners.add(listener);
     }
@@ -64,24 +69,71 @@ public class NegotiationProcess
      * @param listener the listener to remove
      * @return boolean on success
      */
-    public boolean removeEventListener(NegotiationEvent listener)
+    public boolean removeEventListener(NegotiationEventListener listener)
     {
         return listeners.remove(listener);
     }
 
     /**
-     * Begins the negotiation process, presenting the user with the UI.
+     * Initialises the negotiation process and, if successful, presents the UI to the player.
+     * There is a one tick delay before the UI is displayed and the listener is fired to ensure initialisation values are applied.
+     * @throws InvalidParameterException Negotiating entity is unable to negotiate
      */
     public void start()
     {
+        positionEntity(player);
+        positionEntity(mob);
+        mob.setTarget(null); // Disabling this allows us to override the head position of the entity
+        mob.setRotation(mob.getLocation().getYaw(), 45); // Sad expression
+
+        MobNegotiationPlugin plugin = MobNegotiationPlugin.getInstance();   // Wait a tick to allow initialisations to apply
+        plugin.getServer().getScheduler().runTaskLater(plugin, this::beginNegotiation, 1);
+    }
+
+    /**
+     * Presents the negotiation UI to the user and marks the negotiation state as "STARTED"
+     */
+    private void beginNegotiation()
+    {
         this.state = NegotiationState.STARTED;
         this.updateListeners();
-        player.sendTitle(ChatColor.RED + "" + ChatColor.BOLD + "WAIT, WAIT!", null, 2, 20, 7);
-        Bukkit.getScheduler().runTaskLater(MobNegotiationPlugin.getInstance(), () ->
+
+        String alertFormat = "" + ChatColor.RED + ChatColor.BOLD;
+        player.sendTitle(alertFormat + "WAIT, WAIT!", null, 2, 20, 7);
+
+        Bukkit.getScheduler().runTaskLater(MobNegotiationPlugin.getInstance(), this::stop, 80);
+    }
+
+    /**
+     * Teleports the entity to a valid position within their current X/Y co-ordinates for negotiation.
+     * @param entity the entity to position
+     * @throws InvalidParameterException no valid position could be found for the entity
+     */
+    private void positionEntity(Entity entity)
+    {
+        World world = entity.getWorld();
+        Location entityLocation = entity.getLocation();
+        int playerY = entityLocation.getBlockY();
+        for (int yIndex = playerY; yIndex >= player.getWorld().getMinHeight(); yIndex--)
         {
-            this.state = NegotiationState.FINISHED;
-            this.updateListeners();
-        }, 80);
+            Block block = world.getBlockAt(entityLocation.getBlockX(), yIndex, entityLocation.getBlockZ());
+            if (block != null && block.getType().isSolid())
+            {
+                Location teleportLocation = new Location(world, entityLocation.getX(), (yIndex + 1), entityLocation.getZ(), entityLocation.getYaw(), entityLocation.getPitch());
+                entity.teleport(teleportLocation);
+                return;
+            }
+        }
+        throw new InvalidParameterException(String.format("Entity %s is not in a valid negotiating position (%s)", entity.getName(), entity.getLocation()));
+    }
+
+    /**
+     * Forces negotiations to immediately stop.
+     */
+    public void stop()
+    {
+        this.state = NegotiationState.FINISHED;
+        this.updateListeners();
     }
 
     /**
@@ -89,7 +141,7 @@ public class NegotiationProcess
      */
     private void updateListeners()
     {
-        for (NegotiationEvent listener : listeners)
-            listener.onNegotiationStateUpdate(this);
+        for (int i = listeners.size() - 1; i >= 0; i--)
+            listeners.get(i).onNegotiationStateUpdate(this);
     }
 }
