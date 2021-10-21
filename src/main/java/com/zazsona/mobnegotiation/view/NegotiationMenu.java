4270980922c5b5a2232704d834af5a383a0213a1
Zazsona <40435178+Zazsona@users.noreply.github.com>
2021-10-21 19:31:55 +0100

    Add NegotiationMenu view

---
+++ src/main/java/com/zazsona/mobnegotiation/view/NegotiationMenu.java
@@ -0,0 +1,152 @@
+package com.zazsona.mobnegotiation.view;
+
+import com.zazsona.mobnegotiation.view.interfaces.IContainerNegotiationView;
+import com.zazsona.mobnegotiation.view.interfaces.INegotiationView;
+import com.zazsona.mobnegotiation.view.interfaces.IViewInteractionExecutor;
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.chat.BaseComponent;
+import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.api.chat.ComponentBuilder;
+
+import java.util.*;
+
+public class NegotiationMenu implements IContainerNegotiationView
+{
+    private String id;
+    private ArrayList<INegotiationView> children;
+    private String headerText;
+    private ChatColor headerTextColour;
+    private IViewInteractionExecutor interactionExecutor;
+
+    public NegotiationMenu(IViewInteractionExecutor interactionExecutor)
+    {
+        this.id = UUID.randomUUID().toString();
+        this.children = new ArrayList<>();
+        this.headerText = null;
+        this.headerTextColour = ChatColor.GRAY;
+        this.interactionExecutor = interactionExecutor;
+        this.interactionExecutor.addView(this);
+    }
+
+    @Override
+    public INegotiationView getParent()
+    {
+        return null;
+    }
+
+    @Override
+    public String getId()
+    {
+        return id;
+    }
+
+    @Override
+    public INegotiationView getChild(String id)
+    {
+        for (INegotiationView view : children)
+        {
+            if (view.getId().equals(id))
+                return view;
+        }
+        return null;
+    }
+
+    @Override
+    public Collection<INegotiationView> getChildren()
+    {
+        return children;
+    }
+
+    @Override
+    public boolean hasChild(String id)
+    {
+        return getChild(id) != null;
+    }
+
+    @Override
+    public boolean hasChild(INegotiationView view)
+    {
+        return hasChild(view.getId());
+    }
+
+    @Override
+    public boolean addChild(INegotiationView child)
+    {
+        children.add(child);
+        return true;
+    }
+
+    @Override
+    public boolean removeChild(INegotiationView child)
+    {
+        return children.remove(child);
+    }
+
+    /**
+     * Sets the header text to display above the items.
+     * @param text the text to display
+     * @return self
+     */
+    public NegotiationMenu setHeaderText(String text)
+    {
+        this.headerText = text;
+        return this;
+    }
+
+    /**
+     * Sets the colour for the header text to display above the items.
+     * @param colour the header text colour
+     * @return self
+     */
+    public NegotiationMenu setHeaderTextColour(ChatColor colour)
+    {
+        this.headerTextColour = colour;
+        return this;
+    }
+
+    /**
+     * Creates a representation of this prompt as an array of BaseComponents for presentation to users
+     * @return a {@link BaseComponent} array
+     */
+    public BaseComponent[] getFormattedComponent()
+    {
+        ComponentBuilder promptBuilder = new ComponentBuilder().append("\n");
+        if (headerText != null)
+            promptBuilder.append(headerText).append("\n\n").color(headerTextColour).reset();
+
+        /*
+        HashMap<NegotiationResponseType, Integer> typePriority = new HashMap<>();
+        typePriority.put(NegotiationResponseType.SPEECH, 2);
+        typePriority.put(NegotiationResponseType.ATTACK, 1);
+        typePriority.put(NegotiationResponseType.CANCEL, 0);
+
+        List<INegotiationView> sortedChildren = new ArrayList<>(getChildren());
+        sortedChildren.sort((primaryView, secondaryView) ->
+                      {
+                          if (primaryView instanceof NegotiationButton && secondaryView instanceof NegotiationButton)
+                          {
+                              NegotiationResponseType primaryType = ((NegotiationButton) primaryView).getItemType();
+                              NegotiationResponseType secondaryType = ((NegotiationButton) secondaryView).getItemType();
+                              return typePriority.get(primaryType).compareTo(typePriority.get(secondaryType));
+                          }
+                          else if ((primaryView instanceof NegotiationButton) && !(secondaryView instanceof NegotiationButton))
+                              return 1;
+                          else if (!(primaryView instanceof NegotiationButton) && (secondaryView instanceof NegotiationButton))
+                              return -1;
+                          else
+                              return 0;
+                      });
+
+         */
+        for (INegotiationView child : getChildren())
+        {
+            promptBuilder
+                    .append("  ")
+                    .append(child.getFormattedComponent(), ComponentBuilder.FormatRetention.ALL)
+                    .event(new ClickEvent(ClickEvent.Action.RUN_COMMAND, String.format("/%s %s %s", NegotiationViewInteractionExecutor.COMMAND_KEY, id, child.getId())))
+                    .append("\n")
+                    .reset();
+        }
+        return promptBuilder.create();
+    }
+}
