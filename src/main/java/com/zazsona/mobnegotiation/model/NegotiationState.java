e35cedc2fda35a2ef687b858e80c79b57d2d51b8
Zazsona <40435178+Zazsona@users.noreply.github.com>
2021-10-22 19:26:11 +0100

    Add error values to NegotiationState

---
+++ src/main/java/com/zazsona/mobnegotiation/model/NegotiationState.java
@@ -0,0 +1,45 @@
+package com.zazsona.mobnegotiation.model;
+
+public enum NegotiationState
+{
+    NONE(false, false),
+    INITIALISING(false, false),
+    STARTED(false, false),
+    ACTIVE_POWER(false, false),
+    ACTIVE_ITEM(false, false),
+    ACTIVE_ATTACK(false, false),
+    FINISHED_POWER(true, false),
+    FINISHED_ITEM(true, false),
+    FINISHED_ATTACK(true, false),
+    FINISHED_CANCEL(true, false),
+    FINISHED_ERROR_INITIALISATION_FAILURE(true, true),
+    FINISHED_ERROR_ENTITY_LOST(true, true),
+    FINISHED_ERROR_UNKNOWN(true, true);
+
+    private final boolean isTerminating;
+    private final boolean isError;
+
+    NegotiationState(boolean isTerminating, boolean isError)
+    {
+        this.isTerminating = isTerminating;
+        this.isError = isError;
+    }
+
+    /**
+     * Gets if this enum value represents a terminating point within negotiations.
+     * @return true is this state ends a negotiation
+     */
+    public boolean isTerminating()
+    {
+        return this.isTerminating;
+    }
+
+    /**
+     * Gets if this enum value represents an erroneous state
+     * @return true is this state results from an error
+     */
+    public boolean isErroneous()
+    {
+        return this.isError;
+    }
+}
