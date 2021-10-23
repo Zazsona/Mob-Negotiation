842dc2d5ebc9aa8b2bc9cda926a99271e91f5295
Zazsona <40435178+Zazsona@users.noreply.github.com>
2021-10-23 20:16:02 +0100

    Add IAction interface

---
+++ src/main/java/com/zazsona/mobnegotiation/model/action/IAction.java
@@ -0,0 +1,17 @@
+package com.zazsona.mobnegotiation.model.action;
+
+import org.bukkit.entity.Mob;
+import org.bukkit.entity.Player;
+
+public interface IAction
+{
+    void execute();
+
+    void addListener(IActionListener listener);
+
+    boolean removeListener(IActionListener listener);
+
+    void stop();
+
+    boolean isActive();
+}
---
+++ src/main/java/com/zazsona/mobnegotiation/model/action/IActionListener.java
@@ -0,0 +1,8 @@
+package com.zazsona.mobnegotiation.model.action;
+
+public interface IActionListener
+{
+    void onActionStart();
+
+    void onActionComplete();
+}
