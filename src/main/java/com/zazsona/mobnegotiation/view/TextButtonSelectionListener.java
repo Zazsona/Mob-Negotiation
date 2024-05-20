de6dc6f89074ebe3f0b342f2d18a832fd0557004
Zazsona <40435178+Zazsona@users.noreply.github.com>
2024-05-20 19:11:01 +0100

    Correct Text Input not working due to mismatched Regex

--- src/main/java/com/zazsona/mobnegotiation/view/TextButtonSelectionListener.java
+++ src/main/java/com/zazsona/mobnegotiation/view/TextButtonSelectionListener.java
@@ -1,7 +1,9 @@
 package com.zazsona.mobnegotiation.view;
 
+import com.zazsona.mobnegotiation.MobNegotiationPlugin;
 import com.zazsona.mobnegotiation.controller.NegotiationController;
 import com.zazsona.mobnegotiation.view.interfaces.INegotiationView;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -9,6 +11,7 @@ import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.Listener;
 
 import java.util.ArrayList;
+import java.util.concurrent.Callable;
 import java.util.regex.Pattern;
 
 public class TextButtonSelectionListener implements Listener
@@ -22,22 +25,25 @@ public class TextButtonSelectionListener implements Listener
     @EventHandler(priority = EventPriority.LOW)
     public void onPlayerChatAsync(AsyncPlayerChatEvent e)
     {
-        String messageContents = e.getMessage();
+        String messageContent = e.getMessage();
         Player player = e.getPlayer();
-        if (!Pattern.matches(messageContents, "[0-9]"))
+        if (!Pattern.matches("[0-9]", messageContent))
             return;
         if (!controller.isPlayerNegotiating(player))
             return;
 
         NegotiationMenu menu = controller.getNegotiationMenuForPlayer(player);
         ArrayList<INegotiationView> menuChildren = menu.getChildren();
-        int selectionIndex = (Integer.parseInt(messageContents) - 1);
+        int selectionIndex = (Integer.parseInt(messageContent) - 1);
         if (selectionIndex > menuChildren.size())
             return;
 
         String menuId = menu.getId();
         String selectedChildMenuId = menu.getChildren().get(selectionIndex).getId();
-        player.performCommand(String.format("/%s %s %s", NegotiationViewInteractionExecutor.COMMAND_KEY, menuId, selectedChildMenuId));
+        // Commands must be executed on the main thread...
+        Bukkit.getScheduler().callSyncMethod(MobNegotiationPlugin.getInstance(), () ->
+                player.performCommand(String.format("%s %s %s", NegotiationViewInteractionExecutor.COMMAND_KEY, menuId, selectedChildMenuId))
+        );
 
         // Cancel the message to prevent it getting broadcast
         e.setCancelled(true);
