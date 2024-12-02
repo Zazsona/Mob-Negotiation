320c2713d1163cda55e094282b6948072c55a10d
Zazsona <40435178+Zazsona@users.noreply.github.com>
2024-12-02 22:57:24 +0000

    Add HoldUpPrompt table & content

---
+++ src/main/resources/db/migration/V1.13.1__Insert_Data_HoldUpPrompt.sql
@@ -0,0 +1,26 @@
+INSERT INTO HoldUp (HoldUpPromptId, ScriptLineId, PersonalityId)
+VALUES
+       -- Creeper
+       (0, 0, 1) -- "How about we talk thisss out?"
+      ,(1, 1, 2) -- "Pleassse, pleassse... Spare me."
+      ,(2, 2, 3) -- "Ssstop! Ssstop already! What will make you go away?"
+      ,(3, 3, 4) -- "Look, jussst... End me."
+
+       -- Skeleton
+      ,(4, 4, 1) -- "HeY, hOw abOuT we RatTle thIs oNe ouT?"
+      ,(5, 5, 2) -- "Ha- HaNg oN..."
+      ,(6, 6, 3) -- "Why aRe ya raTtlIn' mY boNes?"
+      ,(7, 7, 4) -- "It waSn't quIte mEanT to tuRn oUt liKe thiS..."
+
+       -- Spider
+      ,(8, 8, 1) -- "The %PERSONALITY% %NAME% is staring at you, with a glint in its eye."
+      ,(9, 9, 2) -- "The %PERSONALITY% %NAME% is trembling..."
+      ,(10, 10, 3) -- "The %PERSONALITY% %NAME% appears antsy..."
+      ,(11, 11, 4) -- "The %PERSONALITY% %NAME% seems unfocused..."
+
+       -- Zombie
+      ,(12, 12, 1) -- "Buuhhh... Want... Chat instead?"
+      ,(13, 13, 2) -- "... Spare life?"
+      ,(14, 14, 3) -- "Guuuh... Stop..."
+      ,(15, 15, 4) -- "Buhhh..."
+;
