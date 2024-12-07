6c106d3ba9fe40bab22206816f7deda11927cafe
Zazsona <40435178+Zazsona@users.noreply.github.com>
2024-12-07 21:07:57 +0000

    Add PowerNegotiationPromptEntities table & content

---
+++ src/main/resources/db/migration/V1.30.1__Insert_Data_PowerNegotiationPromptEntities.sql
@@ -0,0 +1,22 @@
+INSERT INTO PowerNegotiationPromptEntities (RootPowerNegotiationPromptId, EntityTypeId)
+VALUES
+        -- Creeper
+        (0, 13) -- Destruction is what you ssseek? I thought humans were creatorsss?
+       ,(75, 13) -- My power? Why? You've already hit me hard...
+       ,(150, 13) -- More humansss after my power? Ugh, why do I always find myssself in thisss sssorry ssstate?
+
+       -- Skeleton
+       ,(300, 14) -- YoU waNtiN' mY poWeR jus' liKe tHat? YoU'lL hAve tO woo mE fIrsT!
+       ,(375, 14) -- I'lL neEd sOmE lunCh firSt. Can wE taKe a bReaK anD go eAt soMethIng?
+       ,(450, 14) -- You wAnt to taKe from Me whIle I'm DowN? DoEs that nOt boTher YouR coNsciEnce?
+
+       -- Spider
+       ,(600, 15) -- The spider doesn't seem to understand...
+       ,(675, 15) -- The spider seems disinterested...
+       ,(750, 15) -- The spider is trying to get away...
+
+       -- Zombie
+       ,(900, 17) -- You want... Power? First tell me... what you want do... with life?
+       ,(975, 17) -- Purgh... Power? How about... propose marriage... first?
+       ,(1050, 17) -- Power...? Do... I see... hand shaking? You... okay?
+;
