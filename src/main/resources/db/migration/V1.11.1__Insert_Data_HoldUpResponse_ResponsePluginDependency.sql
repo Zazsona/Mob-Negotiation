35644bc8c9183e02852d62286cc7dd4117f03413
Zazsona <40435178+Zazsona@users.noreply.github.com>
2024-12-09 23:33:34 +0000

    Fix invalid data for HoldUpResponse's permission & plugin response dependencies

--- src/main/resources/db/migration/V1.11.1__Insert_Data_HoldUpResponse_ResponsePluginDependency.sql
+++ src/main/resources/db/migration/V1.11.1__Insert_Data_HoldUpResponse_ResponsePluginDependency.sql
@@ -1,4 +1,4 @@
-INSERT INTO Response (ResponsePluginDependencyId, ResponseId, PluginNamespacedKey)
+INSERT INTO ResponsePluginDependency (ResponsePluginDependencyId, ResponseId, PluginNamespacedKey)
 VALUES
         (0, 2, 'Vault') -- Give me some money.
 ;
--- src/main/resources/db/migration/V1.12.1__Insert_Data_HoldUpResponse_ResponsePermissionDependency.sql
+++ src/main/resources/db/migration/V1.12.1__Insert_Data_HoldUpResponse_ResponsePermissionDependency.sql
@@ -1,4 +1,7 @@
-INSERT INTO ResponsePermissionDependency (ResponsePluginDependencyId, ResponseId, PermissionKey)
+INSERT INTO ResponsePermissionDependency (ResponsePermissionDependencyId, ResponseId, PermissionKey)
 VALUES
-        (0, 2, 'Vault') -- Give me some money.
+        (0, 0, 'mobnegotiation.negotiation.power') -- "Lend me your power."
+        (1, 1, 'mobnegotiation.negotiation.items') -- "I want items."
+        (2, 2, 'mobnegotiation.negotiation.money') -- "Give me some money."
+        (3, 3, 'mobnegotiation.negotiation.attack') -- "All Out Attack"
 ;
