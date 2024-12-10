84c4373bd008dfebe4f725482ac7c02950b979f2
Zazsona <40435178+Zazsona@users.noreply.github.com>
2024-12-10 23:21:32 +0000

    Add Language

---
+++ src/main/java/com/zazsona/mobnegotiation/model2/Language.java
@@ -0,0 +1,27 @@
+package com.zazsona.mobnegotiation.model2;
+
+public class Language {
+    private String code;
+    private String name;
+
+    public Language(String code, String name) {
+        this.code = code;
+        this.name = name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
