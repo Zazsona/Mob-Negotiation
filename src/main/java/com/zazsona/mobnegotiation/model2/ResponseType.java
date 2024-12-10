9d0e87fc1bc1674d4696febdfb1729a9d1614d3d
Zazsona <40435178+Zazsona@users.noreply.github.com>
2024-12-10 23:21:46 +0000

    Add ResponseType

---
+++ src/main/java/com/zazsona/mobnegotiation/model2/ResponseType.java
@@ -0,0 +1,33 @@
+package com.zazsona.mobnegotiation.model2;
+
+public enum ResponseType
+{
+    PARLAY(0),
+    ATTACK(1),
+    CANCEL(2);
+
+    private int id;
+
+    ResponseType(int id)
+    {
+        this.id = id;
+    }
+
+    public int getId()
+    {
+        return id;
+    }
+
+    /**
+     * Gets the corresponding enum entry by id
+     * @param id the ID to match
+     * @return the {@link ResponseType} with a matching ID, or null if no match found.
+     */
+    public static ResponseType fromId(int id) {
+        for (ResponseType responseType : ResponseType.values()) {
+            if (responseType.getId() == id)
+                return responseType;
+        }
+        return null;
+    }
+}
