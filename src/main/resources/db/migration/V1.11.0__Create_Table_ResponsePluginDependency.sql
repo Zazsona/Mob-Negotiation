d583d1ed67a2af18082c15f6a33db016f84d7b43
Zazsona <40435178+Zazsona@users.noreply.github.com>
2024-12-20 18:39:09 +0000

    PluginNamespaceKey -> PluginKey

--- src/main/resources/db/migration/V1.11.0__Create_Table_ResponsePluginDependency.sql
+++ src/main/resources/db/migration/V1.11.0__Create_Table_ResponsePluginDependency.sql
@@ -2,6 +2,6 @@ CREATE TABLE ResponsePluginDependency
 (
   ResponsePluginDependencyId    INT      NOT NULL     PRIMARY KEY
 , ResponseId                    INT      NOT NULL
-, PluginNamespacedKey           TEXT     NOT NULL
+, PluginKey                     TEXT     NOT NULL
 , FOREIGN KEY(ResponseId) REFERENCES Response(ResponseId)
 );
--- src/main/resources/db/migration/V1.11.1__Insert_Data_HoldUpResponse_ResponsePluginDependency.sql
+++ src/main/resources/db/migration/V1.11.1__Insert_Data_HoldUpResponse_ResponsePluginDependency.sql
@@ -1,4 +1,4 @@
-INSERT INTO ResponsePluginDependency (ResponsePluginDependencyId, ResponseId, PluginNamespacedKey)
+INSERT INTO ResponsePluginDependency (ResponsePluginDependencyId, ResponseId, PluginKey)
 VALUES
         (0, 2, 'Vault') -- Give me some money.
 ;
