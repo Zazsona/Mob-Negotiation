efe95c67ad2a667eb70ba41799724a1052bfc38a
Zazsona <40435178+Zazsona@users.noreply.github.com>
2024-12-21 13:57:21 +0000

    Add CyclicPromptScenario

---
+++ src/main/java/com/zazsona/mobnegotiation/repository/script/CyclicPromptScenario.java
@@ -0,0 +1,9 @@
+package com.zazsona.mobnegotiation.repository.script;
+
+public enum CyclicPromptScenario {
+    INITIAL_OFFER,
+    REVISED_OFFER,
+    REPEAT_OFFER,
+    REJECTION,
+    ACCEPTANCE;
+}
